dcce16982592d068a2d94e3a0f715faf5575fee7
JAMES_666 <1595333805@qq.com>
2025-07-12 18:59:51 +0800

    增加main.cpp文件

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "LinkedList.h"
+
+using namespace std;
+
+int main() {
+    
+    LinkedList<int> list;
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    std::cout << list << std::endl;
+    return 0;
+}
